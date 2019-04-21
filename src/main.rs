74c7d6041f45790876bfa78b7f515c83f37a0ad9
Fenhl <fenhl@fenhl.net>
2019-04-21 16:33:42 +0000

    Add simple CLI

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::{
+    env,
+    process::exit
+};
+use timespec::Error;
+
+fn main() -> Result<(), Error> {
+    let mut args = env::args();
+    let _ = args.next(); // ignore executable name
+    if let Some(date_time) = timespec::next(args)? {
+        println!("{}", date_time);
+        Ok(())
+    } else {
+        eprintln!("no matches found");
+        exit(1);
+    }
+}
